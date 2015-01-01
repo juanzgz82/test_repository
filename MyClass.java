3cfb72e424f9a88e933471515dfcd3ce91d12b0e
juanzgz82 <juan.celtiberia@gmail.com>
2015-01-01 12:26:10 +0100

    issue #1

--- MyClass.java
+++ MyClass.java
@@ -1,11 +1,14 @@
 import utils.Utils;
 
 public class MyClass {
+	final static String SALUDO_EN = "Hello World!";
+	final static String ERROR_EN = "Error: ";
+	
 	public static void main (String[] args) {
 		try {
-			System.out.println("Hello World!");
+			System.out.println(SALUDO_EN);
 		} catch (Exception e) {
-			System.out.println(Utils.getStackTrace(e));
+			System.out.println(ERROR_EN + Utils.getStackTrace(e));
 		}
 	}
 }
