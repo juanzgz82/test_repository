5d55ff7780b3bed015031880d8406b320fba3dcd
juanzgz82 <juan.celtiberia@gmail.com>
2015-01-01 12:42:41 +0100

    cambiado a ES

--- MyClass.java
+++ MyClass.java
@@ -3,7 +3,7 @@ import utils.Utils;
 public class MyClass {
 	public static void main (String[] args) {
 		try {
-			System.out.println("Hello World!");
+			System.out.println("Hola Mundo!");
 		} catch (Exception e) {
 			System.out.println(Utils.getStackTrace(e));
 		}
